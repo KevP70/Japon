713d6f7a4b5db732982bc0a3465e99256b902bb3
Kevp70 <kevin.petronelli@gmail.com>
2020-10-08 13:56:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var button1 = document.getElementById("seasons");
+var button2 = document.getElementById("food");
+var button3 = document.getElementById("otaku");
+
+function saison() {
+    seasons.scrollIntoView(true);
+}
+
+function bouffe() {
+    food.scrollIntoView(true);
+}
+
+function game() {
+    otaku.scrollIntoView(true);
+}
