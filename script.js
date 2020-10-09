3739734010fc0b3653cef652fbdaa4b46fac2013
Kevp70 <kevin.petronelli@gmail.com>
2020-10-09 15:36:05 +0200

    insert slider on responsive

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* button */
+
 var button1 = document.getElementById("seasons");
 var button2 = document.getElementById("food");
 var button3 = document.getElementById("otaku");
@@ -12,4 +14,31 @@ function bouffe() {
 
 function game() {
     otaku.scrollIntoView(true);
+}
+
+/* slider */
+
+var slide = Array("img/fuji.png", "img/tradition.png", "img/modern.png");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+/* slider1 */
+
+var slide1 = new Array("img/arcade.png", "img/Gashapon.png", "img/figure.png", "img/gamecenter.png");
+var numero = 0;
+
+function ChangeSlide1(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide1.length - 1;
+    if (numero > slide1.length - 1)
+        numero = 0;
+    document.getElementById("slide1").src = slide1[numero];
 }
